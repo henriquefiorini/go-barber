4f3cde5bb19116a25e64a4ae2ba521982d541421
Henrique Fiorini <henriquehfr2@gmail.com>
2019-08-22 11:24:50 -0400

    Paginate users appointments listing

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -7,13 +7,17 @@ import File from '../models/File';
 
 class AppointmentController {
   async list(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       where: {
         user_id: req.currentUserId,
         canceled_at: null,
       },
-      order: ['date'],
       attributes: ['id', 'date'],
+      order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
