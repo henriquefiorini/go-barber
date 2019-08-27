1df9338ef3acc5bf9f1d0e1f285d5513181431a3
Henrique Fiorini <henriquehfr2@gmail.com>
2019-08-27 12:19:52 -0400

    Env variables

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: 'cef76d9996ce6a4bf0eec020e06ce625',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '723bb5d3c77160',
-    pass: 'cbafb2c3d7dea7',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'GoBarber <noreply@gobarber.com>',
